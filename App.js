6f3f5eb12ea6d5a483761c11d105916ba0a09dcd
jibontech <jibon.belaface@gmail.com>
2024-04-17 14:47:51 +0600

    added vector icon

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>
+        <Icon name="rocket" size={30} color="#900" />
+      </Text>
     </View>
   );
 };
