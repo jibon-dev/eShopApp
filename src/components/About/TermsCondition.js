72204d7b498b7b3060ef4cf765a8a61769ce1d1d
jibontech <jibon.belaface@gmail.com>
2024-04-23 15:07:12 +0600

    added terms and condation fetch data

---
+++ src/components/About/TermsCondition.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {View, StyleSheet, Platform, useWindowDimensions} from 'react-native';
+import RenderHtml from 'react-native-render-html';
+
+const TermsCondition = ({termsCondition}) => {
+    const {width} = useWindowDimensions()
+    const renderHtmlOptions = {
+        baseStyle: { lineHeight: 25, color:"#000000" }
+    };
+
+    if (termsCondition) {
+        return (
+        <View style={styles.contactContainer}>
+            <View style={styles.contact}>
+                {termsCondition.map((tc, index) => (
+                    <RenderHtml
+                        key={index}
+                        source={{ html: tc?.description }}
+                        contentWidth={width}
+                        {...renderHtmlOptions}
+                    />
+                    ))
+                }
+            </View>
+        </View>
+        );
+    }
+    else{
+        return (
+        <SafeAreaView style={styles.noDataContainer}>
+            <Text style={styles.noDataTitle}>No Data Found</Text>
+        </SafeAreaView>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+  contactContainer: {
+    flex: 1,
+  },
+  contact: {
+    flex: 1,
+    padding: 5,
+    color:"#000",
+    ...Platform.select({
+      ios: {
+        marginBottom: '0%',
+      },
+      android: {
+        marginBottom: 0,
+      },
+    }),
+  },
+  noDataContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  noDataTitle: {
+    fontWeight: 'bold',
+  },
+});
+
+export default TermsCondition;
