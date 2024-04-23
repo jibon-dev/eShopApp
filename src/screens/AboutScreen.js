9f95e9774b361bb0965f0f4cba53f9fca8da0ab9
jibontech <jibon.belaface@gmail.com>
2024-04-23 13:54:40 +0600

    fetch about data

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, ScrollView} from 'react-native';
 import About from '../../src/components/About/About';
+import {BASE_URL} from '../api/api';
 
 const AboutScreen = () => {
+  const [loading, setLoading] = useState(true);
+  const [aboutData, setAboutData] = useState([]);
+
+  useEffect(() => {
+    const fetchAboutData = async () => {
+      try {
+        const res = await fetch(`${BASE_URL}/api/about-us/`);
+        const data = await res.json();
+        setAboutData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+        setLoading(false);
+      }
+    };
+    fetchAboutData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.mainArea}>
-      <About />
+      {loading ? (
+        <Loader />
+      ) 
+      : (
+        <ScrollView>
+          <About aboutData={aboutData}/>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
